a32403e95205256db255e032a18355985223a50c
elayvilkom <elay.vilkom@nitzanim.tech>
2025-06-17 21:35:31 +0300

    Update routes.py

--- app/myapp/routes.py
+++ app/myapp/routes.py
@@ -1,18 +1,22 @@
-from flask import Blueprint, redirect, url_for
-
-from .extensions import db
+from flask import Blueprint, render_template, redirect, url_for, request
 from .models import User
+from .extensions import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
-    users = User.query.all()
-    users_list_html = [f"<li>{ user.username }</li>" for user in users]
-    return f"<ul>{''.join(users_list_html)}</ul>"
+def home():
+    return render_template('home.html') 
+    
+@main.route('/add', methods=['POST'])
+def add_user():
+    username = request.form.get("username")
+    if username:
+        db.session.add(User(username=username))
+        db.session.commit()
+    return redirect(url_for("main.show_users"))
 
-@main.route('/add/<username>')
-def add_user(username):
-    db.session.add(User(username=username))
-    db.session.commit()
-    return redirect(url_for("main.index"))
+@main.route('/users')
+def show_users():
+    users = User.query.all()
+    return f"<ul>{''.join([f'<li>{user.username}</li>' for user in users])}</ul>"
